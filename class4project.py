dd1872173c4e013a8ff008330184e4ffffa4a9d2
javinsingh <javinsinghsethi@gmail.com>
2024-10-06 14:08:53 +0200

    Add files via upload

---
+++ class4project.py
@@ -0,0 +1,10 @@
+import turtle #import library
+turtle.Screen().bgcolor("blue")
+board = turtle.Turtle()
+board.forward(100)
+board.right(90)
+board.forward(100)
+board.right(90)
+board.forward(100)
+board.right(90)
+board.forward(100)
