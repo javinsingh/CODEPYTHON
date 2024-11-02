f2d2d7d43d7506554f032463fa6c5f3a5690571c
javinsingh <javinsinghsethi@gmail.com>
2024-11-02 18:03:06 +0100

    Add files via upload

---
+++ class12.1.py
@@ -0,0 +1,13 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+tuplex = (5, 7, 3, 9, 4, 2)
+print(tuplex)
+tuplex = tuplex + (9,)
+print(tuplex)
+tuple1 = (50, 20, 40, 60, 50)
+print(tuple1.count(50))
+tuplex = (2, 1, 3, 6, 4, 5, 7, 8, 4, 9)
+_slice = tuplex[3:5]
+print(_slice)
+_slice = tuplex[:6]
+print(_slice)
