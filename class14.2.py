0968772eeb04ac59e1366a570e92268cbe11c60c
javinsingh <javinsinghsethi@gmail.com>
2024-11-24 13:47:52 +0100

    Add files via upload

---
+++ class14.2.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+sety = {"blue","yellow"}
+print("original set elements")
+print(setx)
+print(sety)
+print("\n intersection of two said sets")
+setz = setx.intersection(sety)
+print(setz)
